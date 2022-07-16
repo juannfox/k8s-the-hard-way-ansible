030ca23e77f311b6827cb51075fc28c8163efd72
Juan Fox <juannfox@hotmail.com>
2022-07-16 14:25:27 -0300

    Separate containerd+cri into a role

---
+++ install_containerd/molecule/default/bootstrap_pip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Installs all the necessary pip packages. Ideally run this within venv.
+
+molecule_path="molecule/default"
+if [[ $1 ]];then path=$1;fi
+rc=1
+
+if python3 -m pip --version &> /dev/null;then 
+    pip3 install -r "$molecule_path/requirements.txt" --upgrade
+    if [[ $? == 0 ]];then
+        rc=0
+        echo pip3 packages installed. Ready for molecule
+    else
+        echo Error installing pip3 requirements. Aborting...
+        rc=2
+    fi
+else
+    echo pip3 not found. Aborting...
+fi
+
+exit $rc
